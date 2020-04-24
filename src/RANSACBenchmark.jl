c76ad056fb25c4bc89e41e39cecf02b3535599a5
Tamás Cserteg <csertegt@gmail.com>
2020-04-24 11:30:35 +0200

    copy example generation from RANSAC.jl

--- src/RANSACBenchmark.jl
+++ src/RANSACBenchmark.jl
@@ -1,3 +1,25 @@
 module RANSACBenchmark
 
+using LinearAlgebra
+
+using RANSAC
+using StaticArrays
+
+export  sampleplane,
+        sampleplanefromcorner,
+        samplecylinder,
+        samplesphere,
+        samplecone,
+        normalsforplot,
+        noisifyvertices,
+        noisifynormals,
+        makemeanexample,
+        examplepc2,
+        examplepc3,
+        examplepc4,
+        examplepc5,
+        examplepc6
+
+include("exampleclouds.jl")
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,9 @@
 using RANSACBenchmark
+using RANSAC
 using Test
+using LinearAlgebra
+using StaticArrays: SVector
+
+@testset "cone" begin
+    include("cone.jl")
+end
